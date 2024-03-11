8b36555cb7fb8a1f8bd71a0a591fe1cc9dea9803
Hugo Perez <hugoperezgarcia2004@hotmail.com>
2024-03-11 11:24:40 +0100

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import {HeaderInicio} from './components/HeaderInicio.jsx'
-import {ToGame} from './components/ToGame.jsx'
-import {Rules} from './components/Rules.jsx'
-import {LogIn} from './components/LogIn.jsx'
 import {Registro} from './components/Registro.jsx'
 import {RepositorioPartidas} from './components/RepositorioPartidas.jsx'
+import { Home } from './components/Home.jsx'
 
 
 function App() {
   return (
     <>
-      <HeaderInicio />
-      <main className="snap-y snap-mandatory relative w-full h-screen overflow-y-auto overflow-x-hidden scroll-smooth">
-        <div className="snap-center">
-          <ToGame />
-        </div>
-        <div className="snap-center">
-          <Rules />
-        </div>
-        <div className="snap-center">
-          <LogIn />
-        </div>
-      </main>
+      <Home/>
 
 
       {/* <Registro/> */}
---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import { HeaderInicio } from './HeaderInicio.jsx'
+import { ToGame } from './ToGame.jsx'
+import { Rules } from './Rules.jsx'
+import { LogIn } from './LogIn.jsx'
+import React from 'react'
+
+export function Home() {
+    return (
+        <>
+            <HeaderInicio />
+            <main className="snap-y snap-mandatory relative w-full h-screen overflow-y-auto overflow-x-hidden scroll-smooth">
+                <div className="snap-center">
+                    <ToGame />
+                </div>
+                <div className="snap-center">
+                    <Rules />
+                </div>
+                <div className="snap-center">
+                    <LogIn />
+                </div>
+            </main>
+        </>
+    )
+}
