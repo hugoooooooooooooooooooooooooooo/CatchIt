182e49b5bd599daaeffc342ce88ab832632b90b8
hugomorenoo <hugomoreno1614@gmail.com>
2024-03-14 09:49:12 +0100

    Produccion1

--- src/main/java/com/Backend/AtrapaUnMillon/services/PreguntaService.java
+++ src/main/java/com/Backend/AtrapaUnMillon/services/PreguntaService.java
@@ -1,5 +1,7 @@
 package com.Backend.AtrapaUnMillon.services;
 
+import com.Backend.AtrapaUnMillon.exceptions.AdminBadRequestException;
+import com.Backend.AtrapaUnMillon.exceptions.PreguntaBadRequestException;
 import com.Backend.AtrapaUnMillon.models.Admin;
 import com.Backend.AtrapaUnMillon.models.Pregunta;
 import com.Backend.AtrapaUnMillon.repositories.AdminRepository;
@@ -23,6 +25,15 @@ public class PreguntaService {
         return preguntaRepository.findAll();
     }
 
+    public Pregunta getPreguntaById(Long id){
+        Optional<Pregunta> pregunta = preguntaRepository.findById(id);
+        if(pregunta.isPresent()){
+            return pregunta.get();
+        }else{
+            throw new PreguntaBadRequestException("No existe pregunta con ese id");
+        }
+    }
+
     public Pregunta createPregunta(String pregunta, String respuestaCorrecta, String respuesta1,
                                String respuesta2, String respuesta3, String nivel,
                                String dificultad, String asignatura, int tiempo,
@@ -35,8 +46,34 @@ public class PreguntaService {
             preguntaRepository.save(new_pregunta);
             return new_pregunta;
         }else{
-            return null;
-            //Crear excepción
+            throw new AdminBadRequestException("No existe admin");
+        }
+    }
+
+    public Pregunta editPregunta(Pregunta existingPregunta, String pregunta, String respuestaCorrecta,
+                                 String respuesta1, String respuesta2, String respuesta3, String nivel,
+                                 String dificultad, String asignatura, int tiempo, byte[] imagen, Long idAdmin) {
+        Optional<Admin> optionalAdmin = adminRepository.findById(idAdmin);
+        if(optionalAdmin.isPresent()){
+            Admin admin = optionalAdmin.get();
+            if(admin == existingPregunta.getAdmin()){
+                existingPregunta.setPregunta(pregunta);
+                existingPregunta.setRespuestaCorrecta(respuestaCorrecta);
+                existingPregunta.setRespuesta1(respuesta1);
+                existingPregunta.setRespuesta2(respuesta2);
+                existingPregunta.setRespuesta3(respuesta3);
+                existingPregunta.setNivel(nivel);
+                existingPregunta.setDificultad(dificultad);
+                existingPregunta.setAsignatura(asignatura);
+                existingPregunta.setTiempo(tiempo);
+                existingPregunta.setImagen(imagen);
+                preguntaRepository.save(existingPregunta);
+                return existingPregunta;
+            }else{
+                throw new PreguntaBadRequestException("No coincide el admin creador con el que edita");
+            }
+        }else{
+            throw new AdminBadRequestException("No existe admin");
         }
     }
 }
